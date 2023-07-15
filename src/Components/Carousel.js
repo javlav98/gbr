cf9381d7a8ffa19d52c0491ae2b32b3045758ebf
javlav98 <jrlavin98@gmail.com>
2023-07-14 17:57:42 -0700

    removed legend

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -8,35 +8,27 @@ const CarouselExample = () => {
     <Carousel className='custom-carousel'>
       <div className='img'>
         <img src="img1.png" alt="Image 1" />
-        <p className="legend">Image 1</p>
       </div>
       <div className='img'>
         <img src="img2.png" alt="Image 2" />
-        <p className="legend">Image 2</p>
       </div>
       <div className='img'>
         <img src="img3.png" alt="Image 3" />
-        <p className="legend">Image 3</p>
       </div>
       <div className='img'>
         <img src="img4.png" alt="Image 4" />
-        <p className="legend">Image 4</p>
       </div>
         <div className='img'>
         <img src="img5.png" alt="Image 5" />
-        <p className="legend">Image 5</p>
         </div>
         <div className='img'>
         <img src="img6.png" alt="Image 6" />
-        <p className="legend">Image 6</p>
         </div>
         <div className='img'>
         <img src="img7.png" alt="Image 7" />
-        <p className="legend">Image 7</p>
         </div>
         <div className='img'>
         <img src="img8.png" alt="Image 8" />
-        <p className="legend">Image 8</p>
         </div>
     </Carousel>
   );
